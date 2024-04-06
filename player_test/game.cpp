a26fb120670371440cb5649c9ee318a053ce4af4
ChenSpike <e14096083@gs.ncku.edu.tw>
2024-04-07 01:04:53 +0800

    First save for bulid player.

---
+++ player_test/game.cpp
@@ -0,0 +1,31 @@
+#include "game.h"
+#include <QTimer>
+#include <QGraphicsTextItem>
+#include <QFont>
+#include <QBrush>
+#include <QImage>
+
+Game::Game(QWidget *parent){
+    // create the scene
+    scene = new QGraphicsScene();
+    scene->setSceneRect(0,0,800,600); // make the scene 800x600 instead of infinity by infinity (default)
+    setBackgroundBrush(QBrush(QImage(":/images/bg.png")));
+
+    // make the newly created scene the scene to visualize (since Game is a QGraphicsView Widget,
+    // it can be used to visualize scenes)
+    setScene(scene);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setFixedSize(800,600);
+
+    // create the player
+    player = new Player();
+    player->setPos(400,500); // TODO generalize to always be in the middle bottom of screen
+    // make the player focusable and set it to be the current focus
+    player->setFlag(QGraphicsItem::ItemIsFocusable);
+    player->setFocus();
+    // add the player to the scene
+    scene->addItem(player);
+
+    show();
+}
